2c54d5acd8e05aab327ce3f35e243458d4887298
Max Kasprzik <m.kasprzik04@gmail.com>
2019-06-10 09:55:15 -0700

    rename test

--- test2.py
+++ test2.py
@@ -2,4 +2,5 @@ print('this is a second test file')
 # making changes
 # change2
 # change3
+# change5
 # change4stash
--- testrename.py
+++ testrename.py
@@ -2,4 +2,5 @@ print('this is a test file')
 # making change
 # making new change
 # change 3
+# change 5
 # change4
