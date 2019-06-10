bec48d6d0f16ed4637398264cfb2cef0bd9ed11d
Max Kasprzik <m.kasprzik04@gmail.com>
2019-06-10 14:43:46 -0700

    popped

--- test2.py
+++ test2.py
@@ -2,7 +2,11 @@ print('this is a second test file')
 # making changes
 # change2
 # change3
+<<<<<<< Updated upstream
 # change5
 # change4stash
 # change6 - test move
 # change 7
+=======
+# change4
+>>>>>>> Stashed changes
--- testrename.py
+++ testrename.py
@@ -3,7 +3,10 @@ print('this is a test file')
 # making new change
 # change 3
 <<<<<<< Updated upstream:testrename.py
+<<<<<<< Updated upstream:testrename.py
 # change 5
+=======
+>>>>>>> Stashed changes:test.py
 =======
 >>>>>>> Stashed changes:test.py
 # change4
